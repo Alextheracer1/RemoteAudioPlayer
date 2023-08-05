76981060b067d421a984e75a6de386ecd0210f3d
Alextheracer1 <alex.ander.graf@icloud.com>
2023-08-05 21:56:19 +0200

    Initial Commit

---
+++ src/main/java/com/alextheracer1/RemoteAudioPlayer/RemoteAudioPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.alextheracer1.RemoteAudioPlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteAudioPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteAudioPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alextheracer1/RemoteAudioPlayer/RemoteAudioPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alextheracer1.RemoteAudioPlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteAudioPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
